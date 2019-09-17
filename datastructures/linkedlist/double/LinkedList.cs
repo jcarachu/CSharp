b32f9eec27b96370ebd15ac9b56d1f2f4393c69c
Julian Carachure <jcarachu@outlook.com>
2019-09-17 13:30:49 -0700

    finished double linked list generic

--- datastructures/linkedlist/double/LinkedList.cs
+++ datastructures/linkedlist/double/LinkedList.cs
@@ -11,5 +11,152 @@ namespace DoubleLinkedList
 		{
 			AddFirst(new Node<T>(value));
 		}
+		
+		public void AddFirst(Node<T> node)
+		{
+			Node<T> temp = Head;
+			Head = node;
+			Head.Next = temp;
+			if (Count == 0)
+				Tail = Head;
+			else
+				temp.Previous = Head;
+			Count++;
+		}
+		
+		public void AddLast(T value)
+		{
+			AddLast(new Node<T>(value));
+		}
+		
+		public void AddLast(Node<T> node)
+		{
+			if (Count == 0)
+				Head = node;
+			else
+			{
+				Tail.Next = node;
+				node.Previous = Tail;
+			}
+			Tail = node;
+			Count++;
+		}
+		#endregion
+		
+		#region Remove
+		public void RemoveFirst()
+		{
+			if (Count != 0)
+			{
+				Head = Head.Next;
+				Count--;
+				if (Count == 0)
+					Tail = null;
+				else
+					Head.Previous = null;
+			}
+		}
+		
+		public void RemoveLast()
+		{
+			if (Count != 0)
+			{
+				if (Count == 1)
+				{
+					Head = null;
+					Tail = null;
+				}
+				else
+				{
+					Tail.Previous.Next = null;
+					Tail = Tail.Previous;
+				}
+				Count--;
+			}
+		}
+		#endregion
+		
+		#region ICollection
+		public int Count { get; private set; }
+		
+		public void Add(T item)
+		{
+			AddFirst(item);
+		}
+		
+		public bool Contains(T item)
+		{
+			Node<T> current = Head;
+			while (current != null)
+			{
+				if (current.Value.Equals(item))
+					return true;
+				current = current.Next;
+			}
+			return false;
+		}
+		
+		public void CopyTo(T[] array, int arrayIndex)
+		{
+			Node<T> current = Head;
+			while (current != null)
+			{
+				array[arrayIndex++] = current.Value;
+				current = current.Next;
+			}
+		}
+		
+		public bool IsReadOnly { get{ return false; }}
+		
+		public bool Remove(T item)
+		{
+			Node<T> previous = null;
+			Node<T> current = Head;
+			
+			while (current != null)
+			{
+				if (current.Value.Equals(item))
+				{
+					if (previous != null)
+					{
+						previous.Next = current.Next;
+						if (current.Next == null)
+							Tail = previous;
+						else
+							current.Next.Previous = previous;
+						Count--;
+					} else
+					RemoveFirst();
+				return true;
+				}
+				previous = current;
+				current = current.Next;
+			}
+			return false;
+		}
+		
+		System.Collections.Generic.IEnumerator<T> System.Collections.Generic.IEnumerable<T>.GetEnumerator()
+		{
+			Node<T> current = Head;
+			while (current != null)
+			{
+				yield return current.Value;
+				current = current.Next;
+			}
+		}
+		
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return ((System.Collections.Generic.IEnumerable<T>)this).GetEnumerator();
+		}
+		
+		public void Clear()
+		{
+			Head = null;
+			Tail = null;
+			Count = 0;
+		}
+		#endregion
+		
 	}
 }
