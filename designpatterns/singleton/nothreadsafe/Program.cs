dba3b21d005ea0229cf98fd8fa771452d8fc3ad6
Julian Carachure <jcarachu@outlook.com>
2019-10-05 16:56:34 -0700

    singleton pattern implementation with thread safe

---
+++ designpatterns/singleton/nothreadsafe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var insuredName = Policy.Instance.GetInsuredName();
+            Console.WriteLine(insuredName);
+        }
+    }
+}
---
+++ designpatterns/singleton/threadsafe/Policy.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Singleton
+{
+    public class Policy
+    {
+        private static readonly Policy _instance = new Policy();
+        private int Id { get; set; } = 123;
+        private string Insured { get; set; } = "John Roy";
+        public string GetInsuredName() => Insured;
+        public Policy() {}
+        public static Policy Instance {
+            get {
+                return _instance;
+            }
+        }
+    }
+
+    
+
+}
